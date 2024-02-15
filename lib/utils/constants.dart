12550fe2f76fbd20cb897da2ba4c5cf4bc2fe15c
abel3ri <abelmerete@yahoo.com>
2024-02-15 22:30:50 +0300

    Add app icon

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AppConstants {
@@ -22,4 +21,3 @@ String host = Platform.isAndroid ? "10.0.2.2" : "localhost";
 bool useEmulator = false;
 
 FirebaseFirestore db = FirebaseFirestore.instance;
-FirebaseAuth auth = FirebaseAuth.instance;
